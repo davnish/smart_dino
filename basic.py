69a4f7190946c270b00396d3c2754e2ceb60f0cc
davnish <nischal.singh38@gmail.com>
2024-05-24 02:43:49 +0530

    fixed action and ss reading, needs fixing in termination

--- basic.py
+++ basic.py
@@ -1,12 +1,37 @@
 from PIL import ImageGrab
+from PIL import Image
 import numpy as np
 import time
 
-for _ in range(9):
-    ss = ImageGrab.grab(bbox=(0, 400, 1300, 700))
-    ss.save("ss.png")
-    print(np.array(ss))
-    time.sleep(1)
-ss.close()
+# for _ in range(9):
+#     ss = ImageGrab.grab(bbox=(0, 400, 1300, 700))
+#     ss.save("ss.png")
+#     print(np.array(ss))
+#     time.sleep(1)
+# ss.close()
+
+img = Image.open('dino.png')
+img = np.array(img)
+print(img.shape)
+print(img.shape[0]//2, img.shape[1]//2)
+# print(np.array(img)[:,:2])
+# img.show()
+import matplotlib.pyplot as plt
+def isTerminated(state):
+    # print(state)
+    isOver_1 = np.sum([state[578, 347], state[583, 342], state[589, 336], state[596, 329]])
+    print(state[578, 347], state[583, 342], state[589, 336], state[596, 329])
+    over_1 = 1424
+    isOver_2 = np.sum([state[564, 334], state[573, 337], state[591, 336], state[608, 338]])
+    print([state[564, 334], state[573, 337], state[591, 336], state[608, 338]])
+    over_2 = 1424
+    print(isOver_1, isOver_2)
+    if isOver_1 == over_1 or isOver_2 == over_2:
+        return True
+    else: return False
+
+print(isTerminated(img))
+plt.imshow(img)
+plt.show()
 
 
--- train.py
+++ train.py
@@ -1,41 +1,77 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
+# from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import WebDriverException
+from PIL import Image
+import numpy as np
 import time
 
-class WebAgent:
+class WebDino:
     def __init__(self):
         
         self.driver = webdriver.Chrome()
         try: self.driver.get("chrome://dino/")
         except WebDriverException: pass
-        
+
         #---------- Needs exp
         # self.driver.fullscreen_window()
         # time.sleep(1)
-        #----------
+        #---------- 
 
         self.element = self.driver.find_element(By.TAG_NAME, 'body')
+        # self.observation_space = 
+        self.action_space = 3
         # self.actions = ActionChains(self.driver)
 
-    def currState(self):
-        self.element.screenshot('dino.png')
-
     def train(self):
-        self.takeActions()
-        time.sleep(5)
-        self.takeActions()
-        time.sleep(5)
-        self.currState()
-        self.driver.quit()
-    
-    def takeActions(self):
+        # self.takeAction()
+        # time.sleep(1)
+        # print(self.step(self.action_space.sample()))
+        # self.takeAction()
+        # time.sleep(1)
+        # print(self.step())
+        for ep in range(3):
+            self.takeAction(1)
+            time.sleep(2)
 
-        self.element.send_keys(Keys.SPACE)
+            terminated = False
+            while not terminated:
+                action = np.random.choice(self.action_space)
+                currState, terminated = self.step(action)
+                print(terminated)
+
+        self.driver.quit()
+
+    def step(self, action):
+        self.takeAction(action)
+        time.sleep(1)
+        state = self.returnState()
+        return state, self.isTerminated(state)
+        # time.sleep(1)
+    
+    def takeAction(self, action):
+        # print(action)
+        if action == 1: self.element.send_keys(Keys.SPACE)
+        elif action == 2: self.element.send_keys(Keys.ARROW_UP)
+
+    def returnState(self):
+        self.element.screenshot('dino.png') 
+        state = np.asarray(Image.open('dino.png'))
+        return state
+    
+    def isTerminated(self, state):
+        # print(state)
+        isOver_1 = np.sum([state[578, 347], state[583, 342], state[589, 336], state[596, 329]])
+        over_1 = 2052
+        isOver_2 = np.sum([state[564, 334], state[573, 337], state[591, 336], state[608, 338]])
+        over_2 = 2100
+        print(isOver_1, isOver_2)
+        if isOver_1 == over_1 or isOver_2 == over_2:
+            return True
+        else: return False
+
 
 if __name__ == "__main__":
-
-    dino = WebAgent()
+    dino = WebDino()
     dino.train()
